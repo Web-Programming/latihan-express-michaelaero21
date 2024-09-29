7a2cd3afc7cee9bd8c682d797412211f565f9528
Michael <michael_2226240001@mhs.mdp.ac.id>
2024-09-29 21:11:02 +0700

    Tugas Membuat Contact Us
    Tugas pertemuan 6 (27 September 2024)

--- my-express/routes/index.js
+++ my-express/routes/index.js
@@ -10,4 +10,16 @@ router.get("/about", function(rec, res, next){
   res.render('about',{title: 'About Us'});
 });
 
+router.get("/contact", function(rec, res, next){
+  res.render('contact',{title: 'Contact Us'});
+});
+
+/* POST contact form data to thankyou page */
+router.post('/thankyou', function(req, res, next) {
+  // Ambil data dari form contact
+  const { name, email, message } = req.body;
+
+  // Kirim data ke halaman thankyou.ejs
+  res.render('thankyou', { title: 'Thank You', name: name, message: message });
+});
 module.exports = router;
