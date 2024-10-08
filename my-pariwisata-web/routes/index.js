4f464119729955edf0f0959618ce456fab5f47dd
Michael <michael_2226240001@mhs.mdp.ac.id>
2024-10-08 20:02:58 +0700

    Update my-pariwisata-web
    Penambahan Informasi Order Detail

--- my-pariwisata-web/routes/index.js
+++ my-pariwisata-web/routes/index.js
@@ -9,10 +9,10 @@ router.get('/', function(req, res, next) {
 // Updated for Bali tour packages
 router.get('/paketwisata', function(req, res, next) {
   let listpaketwisata = [
-    { 'nama': 'Eksplorasi Pantai Bali', 'harga': 3000000 },
-    { 'nama': 'Heritage Bali', 'harga': 2800000 },
+    { 'nama': 'Eksplorasi Pantai Bali', 'harga': 3500000 },
+    { 'nama': 'Heritage Bali', 'harga': 3000000 },
     { 'nama': 'Petualangan Ubud', 'harga': 5000000 },
-    { 'nama': 'Kuliner Bali', 'harga': 4500000 },
+    { 'nama': 'Kuliner Bali', 'harga': 4000000 },
     { 'nama': 'Wisata Keluarga Bali', 'harga': 3500000 },
     { 'nama': 'Bali Adventure', 'harga': 4000000 }
   ];
@@ -31,10 +31,10 @@ router.post('/orderdetail', function(req, res, next) {
 
   // Calculate total price based on the selected package and number of participants
   const paketHarga = {
-    'Paket Eksplorasi Pantai - Bali': 3000000,
-    'Paket Heritage Bali - Bali': 2800000,
+    'Paket Eksplorasi Pantai - Bali': 3500000,
+    'Paket Heritage Bali - Bali': 3000000,
     'Paket Petualangan Ubud - Bali': 5000000,
-    'Paket Kuliner Bali - Bali': 4500000,
+    'Paket Kuliner Bali - Bali': 4000000,
     'Paket Wisata Keluarga Bali - Bali': 3500000,
     'Paket Bali Adventure - Bali': 4000000
   };
