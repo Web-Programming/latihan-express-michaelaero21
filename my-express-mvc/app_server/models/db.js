b0304427b088b556676d0e89e7134a2bc6de540e
Michael <michael_2226240001@mhs.mdp.ac.id>
2024-10-23 20:34:11 +0700

    Insert

--- my-express-mvc/app_server/models/db.js
+++ my-express-mvc/app_server/models/db.js
@@ -1,4 +1,5 @@
 let mongoose=require("mongoose")
+// let dbURI = "mongodb://localhost:27017/paw-si52"
 let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2"
 
 mongoose.connect(dbURI,{
--- my-express-mvc/app_server/routes/mahasiswas.js
+++ my-express-mvc/app_server/routes/mahasiswas.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const mahasiswaController = require("../controllers/controllerMahasiswa");
 //Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data Dalam Database MongoDB
-router.get("/mahasiswa", mahasiswaController.Index);
+router.get("/mahasiswa", mahasiswaController.index);
+router.post("/mahasiswa/insert", mahasiswaController.insert);
 module.exports = router;
