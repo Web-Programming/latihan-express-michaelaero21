e716f7c5ca59014765acbef2f4eab35958a514d2
Michael <michael_2226240001@mhs.mdp.ac.id>
2024-11-22 14:21:14 +0700

    Housing API

--- my-express-mvc/app.js
+++ my-express-mvc/app.js
@@ -5,11 +5,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 //load mongodb db connection
 require('./app_server/models/db');
-var indexRouter = require('./app_server/routes/index');
 
+var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var app = express();
 var mahasiswasRouter = require('./app_server/routes/mahasiswas');
+var housingRouter = require('./app_server/routes/housing');
+
+var app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'ejs');
@@ -23,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', mahasiswasRouter);
+app.use('/housing', housingRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
