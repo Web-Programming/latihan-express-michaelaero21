6caee180bf02b9f387f45ea5001253fd8528cf5c
Michael <michael_2226240001@mhs.mdp.ac.id>
2024-11-22 14:21:22 +0700

    Update details.component.ts

--- belajar-Angular-Michael_2226240001/src/app/details/details.component.ts
+++ belajar-Angular-Michael_2226240001/src/app/details/details.component.ts
@@ -3,14 +3,41 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormGroup, FormControl, ReactiveFormsModule  } from '@angular/forms';
+
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <p>
-      details works {{ housingLocationId }}!
-    </p>
+     <article >
+      <img class="listing-photo" [src]="housingLocation?.photo"
+        alt="Exterior photo of {{housingLocation?.name}}"/>
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h1 class="section-heading">Apply to live here</h1>
+        <form [formGroup]="applyForm" (submit)="submitApplyForm()">
+          <label for="first-name">First Name</label>
+          <input type="text" id="first-name" formControlName="firstName" placeholder="Input first name">
+          <label for="last-name">Last Name</label>
+          <input type="text" id="last-name" formControlName="lastName" placeholder="Input last name">
+          <label for="email">Email</label>
+          <input type="text" id="email" formControlName="email" placeholder="Input email">
+          <button type="submit" class="primary">Apply</button>
+        </form>
+      </section>
+    </article>
   `,
   styleUrl: './details.component.css'
 })
@@ -19,9 +46,28 @@ export class DetailsComponent {
   housingLocationId = 0;
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined
+  applyForm: FormGroup = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  })
+
   constructor(){
     this.housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId)
     console.table(this.housingLocation)
+  }
+
+  submitApplyForm(){
+    //alert("Hallo you submit a form");
+    //alert("Hallo : " + this.applyForm.value.firstName
+     // + " " + this.applyForm.value.lastName)
+
+    //panggil API simpan data registarsi via service
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    )
   }
 }
